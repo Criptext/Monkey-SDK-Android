3c5ff5adee212718c15a4ee2fa263c774e5b5993
Daniel Tigse <danieltp15@hotmail.com>
2016-03-16 14:52:38 -0500

    nuevo header, nuevo diseno de decrypting y mas cambios de diseno

--- src/main/java/com/criptext/lib/RandomStringBuilder.java
+++ src/main/java/com/criptext/lib/RandomStringBuilder.java
@@ -9,7 +9,7 @@ public class RandomStringBuilder {
     static final String AB = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
     static SecureRandom rnd = new SecureRandom();
 
-    static String build( int len ){
+    public static String build( int len ){
         StringBuilder sb = new StringBuilder( len );
         for( int i = 0; i < len; i++ )
             sb.append( AB.charAt( rnd.nextInt(AB.length()) ) );
